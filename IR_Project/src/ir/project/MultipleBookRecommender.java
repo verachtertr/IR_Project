928ab8aa9e7e72f76a516a15118ea0a4402022c5
Elise Kuylen <elisekuylen@outlook.com>
2016-05-19 17:35:07 +0200

    Different strategies for multiple books.

--- IR_Project/src/ir/project/MultipleBookRecommender.java
+++ IR_Project/src/ir/project/MultipleBookRecommender.java
@@ -5,6 +5,7 @@
  */
 package ir.project;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -27,15 +28,126 @@ public class MultipleBookRecommender {
     }
     
     List<String> getRecommendationsCompareTopK(List<String> userProfile, List<String> userLikes, int top) {
-        return null;
+            HashMap<String, Integer> topRecs = new HashMap();
+            
+            for (int i = 0; i < termMatrix.getNumDocs(); i++) {
+                TFIDFBookVector userBook = termMatrix.getTFIDFVector(i);
+                if (userLikes.contains(userBook.getISBN())) {
+                    HashMap<String, Double> similarities = new HashMap();
+                    for (int j = 0; j < termMatrix.getNumDocs(); j++) {
+                        TFIDFBookVector book = termMatrix.getTFIDFVector(j);
+                        if (!userProfile.contains(book.getISBN())) {
+                            Double sim = userBook.cosineSimilarity(book);
+                            similarities.put(book.getTitle(), sim);
+                        }
+                    }
+                    
+                    HashMap<String, Double> sortedSim = sortByValues(similarities);
+
+                    int t = 0;
+                    for (String key : sortedSim.keySet()) {
+                        if (t >= top) {
+                            break;
+                        }
+                        t++;
+                        if (topRecs.containsKey(key)) {
+                            topRecs.put(key, topRecs.get(key) + 1);
+                        } else {
+                            topRecs.put(key, 1);
+                        }
+                    }
+                }
+            }
+                
+        HashMap<String, Integer> sortedTop = sortByValues(topRecs);
+        List<String> recommendations = new ArrayList();
+        
+        int i = 0;
+        for (String key : sortedTop.keySet()) {
+            if (i >= top) {
+                break;
+            }
+            i++;
+            recommendations.add(key);
+        }
+        return recommendations;
+
     }
     
     List<String> getRecommendationsAddBookVectors(List<String> userProfile, List<String> userLikes, int top) {
-        return null;
+        TFIDFBookVector userBooks = new TFIDFBookVector(termMatrix.getNumTerms());
+        
+        // construct vector with all books combined
+        for (int i = 0; i < termMatrix.getNumDocs(); i++) {
+            TFIDFBookVector book = termMatrix.getTFIDFVector(i);
+            if (userLikes.contains(book.getISBN())) {
+                userBooks.addVector(book);
+            }
+        }
+        
+        // treat this vector as a single 'book'
+        HashMap<String, Double> cosineSimilarities = new HashMap();
+        
+        for (int j = 0; j < termMatrix.getNumDocs(); j++) {
+            TFIDFBookVector book = termMatrix.getTFIDFVector(j);
+            if (! userProfile.contains(book.getISBN())) {
+                Double cosineSimilarity = userBooks.cosineSimilarity(book);
+                cosineSimilarities.put(book.getTitle(), cosineSimilarity);
+            }
+        }
+
+        HashMap<String, Double> sortedSimilarities = sortByValues(cosineSimilarities);
+        List<String> recommendations = new ArrayList();
+        
+        int i = 0;
+        for (String key : sortedSimilarities.keySet()) {
+            if (i >= top) {
+                break;
+            }
+            i++;
+            recommendations.add(key);
+        }
+        return recommendations;
+        
+    
     }
     
     List<String> getRecommendationsAddCosineSimilarities(List<String> userProfile, List<String> userLikes, int top) {
-        return null;
+        HashMap<String, Double> cosineSimilarities = new HashMap();
+        
+        for (int i = 0; i < termMatrix.getNumDocs(); i++) {
+            TFIDFBookVector userBook = termMatrix.getTFIDFVector(i);
+            if (userLikes.contains(userBook.getISBN())) {
+                for (int j = 0; j < termMatrix.getNumDocs(); j++) {
+                    TFIDFBookVector book = termMatrix.getTFIDFVector(j);
+                    if (! userProfile.contains(book.getISBN())) {
+                        Double cosineSimilarity = userBook.cosineSimilarity(book);
+                        
+                        String title = book.getTitle();
+                        
+                        if (cosineSimilarities.containsKey(title)) {
+                            cosineSimilarities.put(title, cosineSimilarities.get(title) + cosineSimilarity);
+                        } else {
+                            cosineSimilarities.put(title, cosineSimilarity);
+                        }
+                    }
+                }
+            }
+        }
+        
+          HashMap<String, Double> sortedSimilarities = sortByValues(cosineSimilarities);
+        List<String> recommendations = new ArrayList();
+        
+        int i = 0;
+        for (String key : sortedSimilarities.keySet()) {
+            if (i >= top) {
+                break;
+            }
+            i++;
+            recommendations.add(key);
+        }
+        return recommendations;
+        
     }
 
     private static HashMap sortByValues(HashMap map) {
@@ -61,97 +173,3 @@ public class MultipleBookRecommender {
     }
 
 }
-
-/*
- //Iterator<JSONObject> ratingsIterator = ratings.iterator();
-            HashMap<String, Double> similarities = new HashMap();
-            //while (ratingsIterator.hasNext()) {
-            //    JSONObject doc = ratingsIterator.next();
-            //    String isbn = (String) doc.get("book_isbn");
-            //    Long rating = (Long) doc.get("score");
-            //    userProfile.add(isbn);
-            //    if (rating >= 4) {
-                    // look up tfidfvector by isbn
-                    for (int i = 0; i < termMatrix.getNumDocs(); i++) {
-                        TFIDFBookVector vec = termMatrix.getTFIDFVector(i);
-                        if (likedBooks.contains(vec.getISBN())) {
-                            // book that user liked
-                            for (int j = 0; j < termMatrix.getNumDocs(); j++) {
-                                TFIDFBookVector book = termMatrix.getTFIDFVector(j);
-                                if(! userProfile.contains(book.getISBN())) {
-                                    String title = book.getTitle();
-                                    Double sim = vec.cosineSimilarity(book);
-                                    if (similarities.containsKey(title)) {
-                                        similarities.put(title, similarities.get(title) + sim);
-                                    } else {
-                                        similarities.put(title, sim);
-                                    }
-                                }
-                            }
-                                
-                        }
-                    }
-              //          if (vec.getISBN().equals(isbn)) {
-              //              
-                            // add to query vector
-                            //likedBooks.addVector(vec);
-                            // get cosine similarities
-                            //HashMap<String, Double> similarities = new HashMap();
-                           // for (int j = 0; j < termMatrix.getNumDocs(); j++) {
-
-                             //   TFIDFBookVector book = termMatrix.getTFIDFVector(j);
-                               // if (!userProfile.contains(book.getISBN())) {
-                                 //   Double sim = vec.cosineSimilarity(book);
-//                                    similarities.put(book.getTitle(), sim);
-//String title = book.getTitle();
-//                                    if(similarities.containsKey(book.getTitle())) {
-//                                        similarities.put(title, similarities.get(title) + sim);
-//                                    } else {
-//                                        similarities.put(title, sim);
-//                                    }
-//                                }
-//                            }
-                           // HashMap<String, Double> sortedSim = sortByValues(similarities);
-
-                            // show first 10
-//                            int t = 0;
-//                            for (String key : sortedSim.keySet()) {
-//                                if (t >= 20) {
-//                                    break;
-//                                }
-//                                t++;
-//                                //if (top.containsKey(key)) {
-//                                //    top.put(key, top.get(key) + 1);
-//                                //} else {
-//                                //    top.put(key, 1);
-//                                // }
-//                            }
-    //                        break;
-  //                      }
-      //              }
-        //        }
-
-          //  }
-            
-            // compare likedBooks vector with other books in the system
-            /*HashMap<String, Double> similarities = new HashMap();
-            for (int i = 0; i < termMatrix.getNumDocs(); i++) {
-                TFIDFBookVector book = termMatrix.getTFIDFVector(i);
-                String isbn = book.getISBN();
-                if (! userProfile.contains(isbn)) {
-                    double sim = likedBooks.cosineSimilarity(book);
-                    similarities.put(book.getTitle(), sim);
-                }
-            }
-            
-            HashMap<String, Double> sortedSimilarities = sortByValues(similarities);
-
-            System.out.println("For you we recommend the following books:");
-            for (String title: sortedSimilarities.keySet()) {
-                System.out.println(title);
-            }
-            //HashMap<String, Integer> sortedTop = sortByValues(top);
-            //for (Map.Entry<String, Integer> entry : sortedTop.entrySet()) {
-            //  System.out.println(entry.getKey()+": "+entry.getValue());
-            //}
-*/
